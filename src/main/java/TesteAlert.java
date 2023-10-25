089792f12b6424d8b94dcd3ca209174a8a0ee626
Miguel Nunes <miguel.npa@gmail.com>
2023-10-25 14:45:56 -0300

    Class Alert, confirm e dismiss

--- src/main/java/TesteAlert.java
+++ src/main/java/TesteAlert.java
@@ -29,4 +29,32 @@ public class TesteAlert {
 		
 		driver.quit();
 	}
+	
+	@Test
+	public void testeDeveInteragirComAlertConfirmSimples() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get(System.getProperty("user.dir") + "\\src\\main\\resources\\componentes.html");
+		
+		WebElement element = driver.findElement(By.id("confirm"));
+		element.click();
+		
+		
+		Alert confirm = driver.switchTo().alert();	
+		Assert.assertEquals("Confirm Simples", confirm.getText());
+		confirm.accept();
+		Assert.assertEquals("Confirmado", confirm.getText());
+		confirm.accept();
+		
+		element.click();
+		
+		
+		confirm = driver.switchTo().alert();	
+		Assert.assertEquals("Confirm Simples", confirm.getText());
+		confirm.dismiss();
+		Assert.assertEquals("Negado", confirm.getText());
+		confirm.dismiss();
+		
+		driver.quit();
+	}
 }
