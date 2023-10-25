9a882f2d06120dcf18efb24cdae056215815b739
Miguel Nunes <miguel.npa@gmail.com>
2023-10-25 20:49:06 -0300

    Testando regras de negócio

--- src/main/java/TesteCadastro.java
+++ src/main/java/TesteCadastro.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
@@ -48,5 +49,93 @@ public class TesteCadastro {
 		
 		driver.quit();
 	
+	}
+	
+	@Test
+	public void testeDeveValidarNomeObrigatorio() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get(System.getProperty("user.dir") + "\\src\\main\\resources\\componentes.html");
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Nome eh obrigatorio", alert.getText());
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void testeDeveValidarSobrenomeObrigatorio() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get(System.getProperty("user.dir") + "\\src\\main\\resources\\componentes.html");
+		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Nome qualquer");
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Sobrenome eh obrigatorio", alert.getText());
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void testeDeveValidarSexoObrigatorio() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get(System.getProperty("user.dir") + "\\src\\main\\resources\\componentes.html");
+		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Nome qualquer");
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Nome qualquer");
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Sexo eh obrigatorio", alert.getText());
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void testeDeveValidarComidaVegetariana() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get(System.getProperty("user.dir") + "\\src\\main\\resources\\componentes.html");
+		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Nome qualquer");
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Nome qualquer");
+		driver.findElement(By.id("elementosForm:sexo:1")).click();
+		
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
+		
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Tem certeza que voce eh vegetariano?", alert.getText());
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void testeDeveValidarEsportistaIndeciso() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get(System.getProperty("user.dir") + "\\src\\main\\resources\\componentes.html");
+		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Nome qualquer");
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Nome qualquer");
+		driver.findElement(By.id("elementosForm:sexo:1")).click();
+		
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		
+		Select combo = new Select(driver.findElement(By.id("elementosForm:esportes")));
+		combo.selectByVisibleText("Karate");
+		combo.selectByVisibleText("O que eh esporte?");
+		
+		
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Voce faz esporte ou nao?", alert.getText());
+		
+		driver.quit();
 	}
 }
