8f5547bbfc2b7a2c53658c4f4d580a60e84de92f
Miguel Nunes <miguel.npa@gmail.com>
2023-10-25 19:49:51 -0300

    Teste Iframe

---
+++ src/main/java/TesteFrames.java
@@ -0,0 +1,33 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteFrames {
+	@Test
+	public void testeDeveInteragirComFrames() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get(System.getProperty("user.dir") + "\\src\\main\\resources\\componentes.html");
+		
+		driver.switchTo().frame("frame1");
+		driver.findElement(By.id("frameButton")).click();
+		
+		Alert alert = driver.switchTo().alert();
+		
+		String text = alert.getText();
+		
+		Assert.assertEquals("Frame OK!", text);
+		
+		alert.accept();
+		
+		driver.switchTo().defaultContent();
+		driver.findElement(By.id("elementosForm:nome")).sendKeys(text);
+		
+		
+		driver.quit();
+	}
+}
